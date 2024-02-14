86870775d33f3110a5790a6b8c5294d34ccfee41
sirnukesalot <123380040+sirnukesalot@users.noreply.github.com>
2024-02-14 21:49:31 +0600

    Add files via upload

---
+++ lab3/10.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let value = prompt('What is the "official" name of JavaScript?', '');
+
+if (value == 'ECMAScript') {
+    alert('Right!');
+} else {
+    alert("You don't know? ECMAScript!");
+}
---
+++ lab3/7.js
@@ -0,0 +1,6 @@
+let a = 2;
+
+let x = 1 + (a *= 2);
+alert(a)
+
+alert(b)
