4cc9209ed26ee729cb1ffd10053cf05a222bab32
sirnukesalot <123380040+sirnukesalot@users.noreply.github.com>
2024-02-29 23:50:33 +0600

    Add files via upload

---
+++ src/www/www.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-www',
+  standalone: true,
+  imports: [],
+  templateUrl: './www.component.html',
+  styleUrl: './www.component.css'
+})
+export class WwwComponent {
+}
