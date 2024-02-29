b83d63ff00ea5ad180a503f761fd288adfdf0294
sirnukesalot <123380040+sirnukesalot@users.noreply.github.com>
2024-02-29 23:58:15 +0600

    Add files via upload

---
+++ lab5/src/products.ts
@@ -0,0 +1,13 @@
+export interface Product{
+  sharelink: string,
+  id:number;
+  name:string;
+  price:string;
+  description:string;
+  imageUrl?:String;
+  link: String;
+  rating: String;
+  categories: String;
+  likes: number;
+}
+
